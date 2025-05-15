a5386a3e7691d4cebe336dc4edcea26b0674d8ae
Darkness <mr.deni_2022@mail.ru>
2025-05-15 14:55:44 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,14 @@
+package jm.task.core.jdbc;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+
+    }
+}
+
+class ChekingConnection{
+    private void trueConnection(){
+
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,9 @@
+package jm.task.core.jdbc.util;
+
+import java.awt.desktop.UserSessionEvent;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+
+
+}
